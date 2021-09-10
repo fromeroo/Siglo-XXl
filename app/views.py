76ee8d26be3d4ef6004fb2496e9169cdbd9962cc
Franco Romero <fromero.olivares01@outlook.com>
2021-09-10 02:35:51 -0300

    [ADD] listUser logic

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import Banco, Usuario
+from .models import Usuario
 from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 
 @login_required
 def indexUser(request):
-    return render(request, 'app/indexUser.html')
+    usuario = Usuario.objects.all()
+    data = {
+        'Usuario': usuario
+    }
+    return render(request, 'app/indexUser.html', data)
 
 def register(request):
     return render(request, 'registration/register.html')
@@ -15,8 +19,8 @@ def index(request):
     return render(request, 'app/index.html')
 
 def administrador(request):
-    Usuario = Usuario.objects.all()
+    usuario = Usuario.objects.all()
     data = {
-        'Usuario': Usuario
+        'Usuario': usuario
     }
-    return render(request, 'app/administrador.html')
+    return render(request, 'app/administrador.html', data)
