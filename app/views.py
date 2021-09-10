df12c2f9ee83c8c19eaaf42463376f7ea843b0ee
Franco Romero <fromero.olivares01@outlook.com>
2021-09-10 00:12:07 -0300

    [add] last changes

--- app/views.py
+++ app/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Banco
-# from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 
+@login_required
 def intranet(request):
     return render(request, 'app/intranet.html')
 
 def register(request):
-    data = {
-        'form': CustomUserCreationForm()
-    }
-    return render(request, 'registration/register.html', data)
+    return render(request, 'registration/register.html')
 
 def index(request):
     return render(request, 'app/index.html')
