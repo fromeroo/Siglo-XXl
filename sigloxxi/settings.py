ea45a545b9338cca9ff8e42dc11958e0ed233556
Franco Romero <fromero.olivares01@outlook.com>
2021-10-09 19:09:45 -0300

    [ADVANCE] logic and consume procedure

--- sigloxxi/settings.py
+++ sigloxxi/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions', 
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
     'crispy_forms',
+    'rest_framework',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
