04a5105663173a3b205df844a8c35c33ded670d3
PatricioArceAraya <85263250+PatricioArceAraya@users.noreply.github.com>
2021-10-13 00:44:07 -0300

    MenuAvance

--- app/views.py
+++ app/views.py
@@ -573,15 +573,15 @@ def dashboard(request):
 
 # TABLET CLIENTE
 def clienteMenu(request):
-
+    messages.success(request, "¡!")
     return render(request, 'app/cliente/clienteMenu.html')
 
 def detalleCliente(request):
-
+    messages.success(request, "¡!")
     return render(request, 'app/cliente/detalleCliente.html')
 
 def pagoCliente(request):
-
+    messages.success(request, "¡!")
     return render(request, 'app/cliente/pagoCliente.html')
 
 
